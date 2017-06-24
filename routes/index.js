1ed6e2e0a89efaf219a059ce19e869c17a964713
danielmoi <daniel@ampersandmoi.com>
2017-06-24 19:16:34 +1000

    Hook up search!

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
 
 const router = express.Router();
 
@@ -8,13 +10,27 @@ router.get('/health', (req, res) => {
 });
 
 
-router.get('/api/imagesearch/:search', (req, res) => {
+router.get('/api/imagesearch/:search', async (req, res) => {
   const { search } = req.params;
   const { query } = req;
   console.log('search:', search);
   console.log('query:', query);
+
+  const qst = qs.stringify({ q: search });
+
+  console.log('process.env.BING_SUBSCRIPTION_KEY:', process.env.BING_SUBSCRIPTION_KEY);
+
+  const result = await axios({
+    method: 'GET',
+    url: `https://api.cognitive.microsoft.com/bing/v5.0/images/search?${qst}`,
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.BING_SUBSCRIPTION_KEY,
+    },
+  });
+  console.log('result:', result);
+
   res.json({
-    it: 'works',
+    result: result.data,
   });
 });
 
